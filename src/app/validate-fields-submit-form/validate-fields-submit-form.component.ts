282bc8f6a09eab67f542ae02ddeb1d907185c045
loiane <loianeg@gmail.com>
2017-08-03 11:05:48 -0300

    ValidateFieldsSubmitFormComponent

--- src/app/validate-fields-submit-form/validate-fields-submit-form.component.ts
+++ src/app/validate-fields-submit-form/validate-fields-submit-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-validate-fields-submit-form',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValidateFieldsSubmitFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required],
+    });
   }
+
+  onSubmit() {}
 
 }
